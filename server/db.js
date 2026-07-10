b7c94aa88d1a76d216ece385ef7d475b263fb87a
Erika Cibelly Santos <105865954+ErikaCibellySx24@users.noreply.github.com>
2026-07-10 21:21:18 +0000

    comit

--- server/db.js
+++ server/db.js
@@ -1,41 +1,16 @@
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
-// 🔥 BANCO ÚNICO E FIXO
 const db = new sqlite3.Database(
   path.resolve(__dirname, "../database/tracker.db")
 );
 
 db.serialize(() => {
 
+  // ==========================
+  // USUÁRIOS
+  // ==========================
   db.run(`
-    CREATE TABLE IF NOT EXISTS cronograma (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      dia TEXT NOT NULL,
-      materia TEXT NOT NULL,
-      concluido INTEGER DEFAULT 0
-    )
-  `);
-
-  db.run(`
-    CREATE TABLE IF NOT EXISTS estudos (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      materia TEXT,
-      horas REAL,
-      data TEXT DEFAULT CURRENT_DATE
-    )
-  `);
-
-  db.run(`
-    CREATE TABLE IF NOT EXISTS questoes (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      materia TEXT,
-      resolvidas INTEGER,
-      acertos INTEGER,
-      data TEXT DEFAULT CURRENT_DATE
-    )
-  `);
-    db.run(`
     CREATE TABLE IF NOT EXISTS usuarios (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       nome TEXT NOT NULL,
@@ -43,6 +18,58 @@ db.serialize(() => {
       senha TEXT NOT NULL,
       areaConcurso TEXT NOT NULL,
       created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    )
+  `);
+
+  // ==========================
+  // CRONOGRAMA
+  // ==========================
+  db.run(`
+    CREATE TABLE IF NOT EXISTS cronograma (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      usuario_id INTEGER NOT NULL,
+      dia TEXT NOT NULL,
+      materia TEXT NOT NULL,
+      concluido INTEGER DEFAULT 0,
+
+      FOREIGN KEY (usuario_id)
+      REFERENCES usuarios(id)
+      ON DELETE CASCADE
+    )
+  `);
+
+  // ==========================
+  // ESTUDOS
+  // ==========================
+  db.run(`
+    CREATE TABLE IF NOT EXISTS estudos (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      usuario_id INTEGER NOT NULL,
+      materia TEXT,
+      horas REAL,
+      data TEXT DEFAULT CURRENT_DATE,
+
+      FOREIGN KEY (usuario_id)
+      REFERENCES usuarios(id)
+      ON DELETE CASCADE
+    )
+  `);
+
+  // ==========================
+  // QUESTÕES
+  // ==========================
+  db.run(`
+    CREATE TABLE IF NOT EXISTS questoes (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      usuario_id INTEGER NOT NULL,
+      materia TEXT,
+      resolvidas INTEGER,
+      acertos INTEGER,
+      data TEXT DEFAULT CURRENT_DATE,
+
+      FOREIGN KEY (usuario_id)
+      REFERENCES usuarios(id)
+      ON DELETE CASCADE
     )
   `);
 
